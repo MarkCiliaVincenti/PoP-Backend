1d4c5b9d5c4ef492d897b59860b43df98ac59622
ebwinters <ebwinters@comcast.net>
2021-10-17 18:29:37 -0700

    add all DB

--- PopApis/PopDb/dbo/Tables/AuctionType.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[AuctionType] (
-    [Id]   INT            IDENTITY (1, 1) NOT NULL,
-    [Type] NVARCHAR (255) NULL,
-    CONSTRAINT [PK_AuctionType] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
